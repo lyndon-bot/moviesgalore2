8fd845ec9b472867bf2d1bf4efad8f39f8eedc49
lyndon bowen <lyndon bowen@DellWin8>
2017-07-09 11:19:29 -0400

    First Commit

---
+++ Functions/header.php
@@ -0,0 +1,9 @@
+<?php
+
+include "config.php";
+include "query.php";
+include "error.php";
+
+
+
+
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<?php
+
+include "/Functions/header.php";
+
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="bootstrap.css" rel="stylesheet">
+        <link href="main.css" rel="stylesheet">
+        <title>Index</title>
+    </head>
+    <body>
+       <nav class="navbar navbar-inverse navbar-fixed-top">
+           
+            <ul class="navbar nav">
+                <li>Actor</li>
+                <li>Title</li>
+                <li>Director</li>
+            </ul>  
+         
+   <?php
+   
+  
+   ?>
+        </nav>
+
+        
+        
+        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="bootstrap.js"></script>
+    </body>
+</html>
