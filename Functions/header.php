7583b1b912d4f8a9c960dacadb60f89f4f8a289c
lyndon bowen <lyndon bowen@DellWin8>
2017-07-09 13:01:06 -0400

    Updated Push

--- Functions/header.php
+++ Functions/header.php
@@ -5,5 +5,13 @@ include "query.php";
 include "error.php";
 
 
+?>
 
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="../bootstrap.css" rel="stylesheet">
+        <link href="../main.css" rel="stylesheet">
+        <title>Index</title>
+    </head>
 
