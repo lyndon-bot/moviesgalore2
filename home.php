409375336d37165e72db99d83236a6dd31787c97
dr <dr@drpesante>
2017-07-12 21:51:30 -0400

    Asiya Commit

--- home.php
+++ home.php
@@ -7,11 +7,21 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Movies Galore</title>
     </head>
     <body>
         <?php
         // put your code here
+        
         ?>
+        <h1>
+            Movies Galore 
+        </h1>
+        <h6>
+            Search Here or Choose a category below
+        </h6>
+        <form>
+            
+        </form>
     </body>
 </html>
