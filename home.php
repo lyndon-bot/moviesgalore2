3206d4ed03ede630d3fec5615012aca07d2ac40a
dr <dr@drpesante>
2017-07-13 21:56:38 -0400

    Asiya Commit

--- home.php
+++ home.php
@@ -5,123 +5,119 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <?php
-
-    include "conn.php";
-    include "query.php";
-
+include "conn.php";
+include "query.php";
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Movies Galore</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        
-        ?>
+<?php
+// put your code here
+?>
         <h1>
             Movies Galore 
         </h1>
         <h6>
             Search Here or Choose a category below
         </h6>
-        
+
         <table>
-            
-        <?php
-            
-            $get = query("Select distinct Genre from dvd");
-            $show = mysqli_fetch_all($get,MYSQLI_ASSOC);
-            
-            foreach($show as $show){
-                
-                echo "<tr> <td>" . $show['Genre'] . "</td></tr>";
-            }
-        
-        
-        ?>
-            
+
+<?php
+$get = query("Select distinct Genre from dvd");
+$show = mysqli_fetch_all($get, MYSQLI_ASSOC);
+
+foreach ($show as $show) {
+
+    echo "<tr> <td>" . $show['Genre'] . "</td></tr>";
+}
+?>
+
         </table>
-            
+
         <div class='form'> </div>
         <ul class='tab-group'>
             <li class='tab active'><a href='#signup'>Sign up</a> </li>
             <li class='tab'> <a href='#login'>log In</a> </li>
         </ul>
-             
-            <div class="tab-content">
-        <div id="signup">   
-          <h1>Sign Up for Free</h1>
-          
-          <form action="/" method="post">
-          
-          <div class="top-row">
-            <div class="field-wrap">
-              <label>
-                First Name<span class="req">*</span>
-              </label>
-              <input type="text" required autocomplete="off" />
-            </div>
-        
-            <div class="field-wrap">
-              <label>
-                Last Name<span class="req">*</span>
-              </label>
-              <input type="text"required autocomplete="off"/>
-            </div>
-          </div>
 
-          <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off"/>
-          </div>
-          
-          <div class="field-wrap">
-            <label>
-              Set A Password<span class="req">*</span>
-            </label>
-            <input type="password"required autocomplete="off"/>
-          </div>
-          
-          <button type="submit" class="button button-block"/>Get Started</button>
-          
-          </form>
+        <div class="tab-content">
+            <div id="signup">   
+                <h1>Sign Up for Free</h1>
+
+                <form action="/" method="post">
+
+                    <div class="top-row">
+                        <div class="field-wrap">
+                            <label>
+                                First Name<span class="req">*</span>
+                            </label>
+                            <input type="text" required autocomplete="off" />
+                        </div>
+
+                        <div class="field-wrap">
+                            <label>
+                                Last Name<span class="req">*</span>
+                            </label>
+                            <input class="form-control" type="text"required autocomplete="off"/>
+                        </div>
+                    </div>
+
+                    <div class="field-wrap">
+                        <label>
+                            Email Address<span class="req">*</span>
+                        </label>
+                        <input  type="email"required autocomplete="off"/>
+                    </div>
+
+                    <div class="field-wrap">
+                        <label>
+                            Set A Password<span class="req">*</span>
+                        </label>
+                        <input type="password"required autocomplete="off"/>
+                    </div>
+
+                    <button type="submit" class="button button-block"/>Get Started</button>
+
+                </form>
+
+            </div>
+
+            <div id="login">   
+                <h1>Welcome Back!</h1>
+
+                <form action="/" method="post">
+
+                    <div class="field-wrap">
+                        <label>
+                            Email Address<span class="req">*</span>
+                        </label>
+                        <input type="email"required autocomplete="off"/>
+                    </div>
+
+                    <div class="field-wrap">
+                        <label>
+                            Password<span class="req">*</span>
+                        </label>
+                        <input type="password"required autocomplete="off"/>
+                    </div>
+
+                    <p class="forgot"><a href="#">Forgot Password?</a></p>
+
+                    <button class="button button-block"/>Log In</button>
+
+                </form>
+
+            </div>
 
         </div>
-        
-        <div id="login">   
-          <h1>Welcome Back!</h1>
-          
-          <form action="/" method="post">
-          
-            <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off"/>
-          </div>
-          
-          <div class="field-wrap">
-            <label>
-              Password<span class="req">*</span>
-            </label>
-            <input type="password"required autocomplete="off"/>
-          </div>
-          
-          <p class="forgot"><a href="#">Forgot Password?</a></p>
-          
-          <button class="button button-block"/>Log In</button>
-          
-          </form>
 
-        </div>
-        
-      </div>
-      
-</div> 
-        </form>
-    </body>
+    </div> 
+</form>
+</body>
 </html>
