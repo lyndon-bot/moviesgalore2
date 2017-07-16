b9dc5302bd4f038facb0c4fe7e5ac304acd481be
dr <dr@drpesante>
2017-07-15 21:35:49 -0400

    Merge origin/master
    Conflicts:
    	index.php
    	main.css
    	nbproject/project.xml

---
+++ cart.php
@@ -0,0 +1,37 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<style>
+    h1{
+        color: #080808;
+        font-family: Arial, helvetica, sans-serif
+    }
+    select{
+        background-color: #ffffff;
+        
+    }
+    body{
+        background-color: #663300;
+    }
+    
+</style>
+<body>
+<h1> Cart </h1>
+<select>
+    <option value="">
+        
+    </option>
+</select>
+<select>
+    <option value="">
+        
+    </option>
+</select>
+<input type="submit" value="Checkout" name="Finish" />
+</body>
